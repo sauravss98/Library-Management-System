af5fac1284109c166b8134f267c11b418c763b99
Inferno0990 <sauravsuresh171@gmail.com>
2022-09-15 18:05:03 +0530

    Completed User section

--- LMS/User/views.py
+++ LMS/User/views.py
@@ -1,3 +1,4 @@
+from ast import Is
 from rest_framework import generics
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated
@@ -5,11 +6,20 @@ from LMS.User.models import Student
 from User.permissions import IsAdmin
 from User.serializers import UserSerializer
 # Create your views here.
-class ListStudents(generics.ListAPIView):
+class ListStudentsDetails(generics.ListAPIView):
     permission_classes=(IsAdmin,)
     serializer_class=UserSerializer
     queryset = Student.objects.all()
 
-class CreateStudent(generics.CreateAPIView):
+class CreateStudentDetails(generics.CreateAPIView):
     permission_classes=(IsAdmin,)
     serializer_class=UserSerializer
+
+class UpdateStudentDetails(generics.UpdateAPIView):
+    permission_classes=(IsAdmin,)
+    serializer_class=UserSerializer
+    queryset=Student.objects.all()
+
+class DeleteStudentDetails(generics.DestroyAPIView):
+    permission_classes=(IsAdmin,)
+    queryset=Student.objects.all()
